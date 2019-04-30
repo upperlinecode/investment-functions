b2b8466cc798a1997031b20d39a206886602ab9f
Jeffrey Lancaster <jeffrey.lancaster@gmail.com>
2019-04-30 20:21:31 +0000

    add invest.py

---
+++ invest.py
@@ -0,0 +1,43 @@
+### Check the README for additional information ###
+
+# 1. Create a function called number_shares_a() that only accepts an investment amount and returns the number of shares of Fund A which that investment could buy today.
+
+
+
+# 2. Create a more-general function called number_shares() that takes in an investment amount and a fund. It should return the number of shares of that fund which that investment could buy today.
+
+
+
+# 3. Create a more flexible calculator method called history_shares() that takes in an investment amount, fund, and year and returns the number of shares that money could buy in that year.
+
+
+
+### Advanced Bonus Challenge ###
+
+# 4. What is the change of an investment over time? Create a method calculate_profit() that accepts an initial investment amount, the fund, and the initial investment year, and returns the current profit made on the initial investment. This is an open ended one - there are many ways to do this!
+
+
+
+# Bonus: Format the output of the function as USD currency, so instead of 1000, the function should output $1000.00.
+
+
+
+### Double Advanced Bonus Challenge ###
+
+# 5. Which investment is the best? Create a method best_choice() that accepts an investment amount and a year, and returns the name of the fund that would have made the most money for the investor.
+
+
+
+# Bonus: In addition to the name of the fund, also return how much money the investor would have earned.
+
+
+
+### Triple Advanced Bonus Challenge ###
+
+# 6. Consider the current profit difference between making an investment in a fund several years apart. Create a method investment_diff() that accepts an investment amount, a fund, and two year values and returns the current profit difference if the money had been made earlier vs. later.
+
+
+
+# Bonus: How can you account for a sloppy investment advisor who puts the years in a different order than you expect?
+
+
